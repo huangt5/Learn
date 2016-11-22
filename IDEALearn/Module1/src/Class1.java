11710b3e4cd2ec33153c0ac0ee015444a080dbdf
Terry Huang <terry_h@126.com>
2016-11-22 15:55:53 +0800

    Add IDEA learn.

---
+++ IDEALearn/Module1/src/Class1.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 11/22/2016.
+ */
+public class Class1 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Class1");
+        System.in.read();
+    }
+}
---
+++ IDEALearn/src/Class2.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 11/22/2016.
+ */
+public class Class2 {
+    public static void main(String[] args) throws IOException {
+        System.out.print("Class2");
+        System.in.read();
+    }
+}
